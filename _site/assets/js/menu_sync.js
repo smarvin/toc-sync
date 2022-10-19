fbafcd76cb5993d4c47e44b3a81cf8ac084e8da6
smarvin <scot.marvin@salesforce.com>
2022-10-19 14:55:10 -0700

    Cleaning up

--- _site/assets/js/menu_sync.js
+++ _site/assets/js/menu_sync.js
@@ -3,7 +3,9 @@
         var menuItem = document.getElementsByClassName('menu_active')[0].parentElement;
         var menuItemTopLocation = menuItem.offsetTop;
 
-        // Now we tell the div to scroll to that position using scrollTop:
-        document.getElementById('docs_menu_container').scrollTop = menuItemTopLocation;
+        var container = document.getElementById('docs_menu_container')
+        var newLocation = menuItemTopLocation - (container.offsetHeight - menuItem.scrollHeight - 50);
+
+        container.scrollTop = newLocation;
     });
 })();
